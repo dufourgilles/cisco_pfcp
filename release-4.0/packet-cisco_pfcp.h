fb1b812856d76ef1902c896c5afa6c50fc770dcb
Gilles Dufour <dufour.gilles@gmail.com>
2022-10-12 10:21:00 +0200

    Support for version 4.0 with pfcp content tlv

---
+++ release-4.0/packet-cisco_pfcp.h
@@ -0,0 +1,19 @@
+
+#ifndef _PACKET_CISCO_PFCP_H_
+#define _PACKET_CISCO_PFCP_H_
+
+typedef struct pfcp_session_args {
+    wmem_list_t *seid_list;
+    wmem_list_t *ip_list;
+    guint64 last_seid;
+    address last_ip;
+    guint8 last_cause;
+} pfcp_session_args_t;
+
+static int ett_pfcp = -1;
+static expert_field ei_pfcp_ie_data_not_decoded = EI_INIT;
+
+
+
+
+#endif
